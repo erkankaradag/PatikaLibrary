b00dbccd65201c6162b1425fc538e0a24667989e
erkankaradag <erkankaradagcom@gmail.com>
2025-01-09 23:54:09 +0300

    Add project files.

---
+++ PatikaLibrary/Books.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    public class Books   // KİTAP NESNESİ ŞABLONU   ----->   C#'ta class (sınıf), nesne tabanlı programlamanın temel yapı taşıdır. Sınıflar, veri (alanlar/fields) ve davranışları (metotlar/methods) bir arada tanımlayan şablonlardır. Sınıflar kullanılarak nesneler (objects) oluşturulur.
+    {
+        private string bookName;            //  {
+        private string authorName;          //
+        private string authorSurname;       //      
+        private int numberOfPages;          //      Field'lar
+        private string publishingHouse;     //
+        private DateTime registrationDate;  //   }   ----> C#'da field (alan), bir sınıf içinde değişkenlerin tanımlandığı yerlerdir. Field'lar, bir sınıfın durumunu (state) saklamak için kullanılır ve genellikle özel (private) olarak tanımlanır. Field'lara genellikle sınıfın metotları veya özellikleri (property) aracılığıyla erişilir.
+
+
+        public Books() //Parametresiz Constructor,   
+        {
+            RegistrationDate = DateTime.Now;
+        }
+
+        // C#'ta parametre almayan constructor (yapıcı metot), bir sınıfın örneği oluşturulduğunda varsayılan işlemleri gerçekleştiren ve herhangi bir parametre almayan bir metottur. Bu tür constructor'lar genellikle sınıf üyelerini varsayılan değerlere ayarlamak için kullanılır.
+
+        public Books(string bookName, string authorName, string authorSurname, int numberOfPages, string publishingHouse) // Parametreli Constructor
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            NumberOfPages = numberOfPages;
+            PublishingHouse = publishingHouse;
+            RegistrationDate = DateTime.Now;
+        }
+        // C#'ta parametre alan constructor (yapıcı metot), bir sınıfın örneği oluşturulurken başlangıç değerlerini belirlemek için kullanılan ve parametre kabul eden bir metottur. Bu, nesnelerin özel ihtiyaçlara göre oluşturulmasını sağlar.
+
+
+        //PROPERTY'LER -----> C#'ta property (özellik), sınıfların (class) içinde tanımlanan ve genellikle sınıfın bir alanına (field) erişimi kontrol etmek için kullanılan bir yapıdır. Property'ler, bir sınıfın dışarıya bir değer sunmasını (get) veya dışarıdan bir değer almasını (set) sağlar. Property kullanımı, veri doğrulama ve gizlilik sağlamada önemlidir.
+        public string BookName
+        {
+            get { return bookName; }
+            set { bookName = value; }
+        }
+        public string AuthorName
+        {
+            get { return authorName; }
+            set { authorName = value; }
+        }
+        public string AuthorSurname
+        {
+            get { return authorSurname; }
+            set { authorSurname = value; }
+        }
+        public int NumberOfPages
+        {
+            get { return numberOfPages; }
+            set { numberOfPages = value; }
+        }
+
+        public string PublishingHouse
+        {
+            get { return publishingHouse; }
+            set { publishingHouse = value; }
+        }
+
+        public DateTime RegistrationDate
+        {
+            get { return registrationDate; }
+            set { registrationDate = DateTime.Now; }
+        }
+
+        // Field'ları kullanan bir metot
+        public void BookInformation()
+        {
+            Console.WriteLine($"Kitap Adı: {bookName}, Yazar Adı ve Soyadı: {authorName} {authorSurname}, Sayfa Sayısı: {numberOfPages}, Yayınevi: {publishingHouse}, Kayıt Tarihi: {registrationDate} ");
+        }
+
+    }
+}
---
+++ PatikaLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+namespace PatikaLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books book1 = new Books();
+
+            book1.BookName = "İdamlık Genç";
+            book1.AuthorName = "Emine";
+            book1.AuthorSurname = "Şenlikoğlu";
+            book1.NumberOfPages = 250;
+            book1.PublishingHouse = "Etiket";
+            book1.BookInformation();
+
+            Console.WriteLine("-------------------- 2. BOOK -----------------------------");
+
+            Books books2 = new Books("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+            books2.BookInformation();
+
+            Console.WriteLine("-----------------------");
+
+        }
+    }
+}
+
+
